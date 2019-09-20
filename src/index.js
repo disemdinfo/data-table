7c6e351cbbab8122282420aa1915b51e56733d63
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-09-20 16:32:28 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import DataTable from './DataTable';
-export default DataTable;
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import DataTable from './DataTable';
+
+ReactDOM.render(<h1 columns={{ a: 'A'}} rows={[{ a: 'xxx' }, { a: 'xxx' }]} >xxxxx</h1>, document.getElementById('root'));
